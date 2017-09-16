295af1347757ca10202c387be9bc7e62d973380d
Mariamma Antony <Mariamma.Antony@aexp.com>
2017-09-16 20:41:53 +0530

    adding filter for place names

--- js/app.js
+++ js/app.js
@@ -1,34 +1,14 @@
-var initialLocations=[{
-	locationName: "North Bergen"
-},
-{
-	locationName: "Englewood"
-},
-{
-	locationName: "Brooklyn"
-},
-{
-	locationName: "Bayonne"
-},
-{
-	locationName: "Maplewood"
-},
-{
-	locationName: "Irvington"
-},
-{
-	locationName: "Elizabeth"
-},
-{
-	locationName: "Summit"
-},
-{
-	locationName: "Harrison"
-}
+var initialLocations=[{title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+          {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+          {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+          {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+          {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+          {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
 ];
 
 var Location = function(data){
-	this.locationName = ko.observable(data.locationName);
+	this.title = ko.observable(data.title);
+	this.location = ko.observable(data.location);
 }
 
 var ViewModel = function(){
